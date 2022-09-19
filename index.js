15f69a2ae2aaf7bb9f5ed938bf97b30cf51ebbd0
Michael Orozco <colomike925@gmail.com>
2022-09-19 07:42:48 -0400

    commit tree 705d6a25b9625c703bcbbebc0b160e5ba36e9228

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+
+function writeCards(array, event) {
+    const newArr = [];
+    for(let i = 0; i < array.length; i++) {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return newArr;
+}
+
+function countDown(number) {
+    let countdown = number;
+    while(countdown >= 0) {
+        console.log(countdown--);
+
+    }
+}
